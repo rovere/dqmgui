58a87b2586bcc85a3c9695f3aa2d395b9682d027
Colin Nolan <cn580@york.ac.uk>
2013-09-06 13:53:50 +0200

    - Fixed issue encountered with g++ compiler - 'typename' keyword required before generic typed 'std::vector::iterator' used. - Extended HistogramData so that it is able to store the associated histogram's draw options. - Added cmath library include in HistogramScalingUtil (not included as standard in g++ compilation). - Sorted includes in StackedHistogramBuilder.

--- src/cpp/DQM/render/models/HistogramData.h
+++ src/cpp/DQM/render/models/HistogramData.h
@@ -7,29 +7,43 @@
 #ifndef HISTOGRAMDATA_H_
 #define HISTOGRAMDATA_H_
 
+#include <string>
+
 class TH1;
 
 namespace render {
 	/// Model of data required to display a histogram.
 	class HistogramData {
 		private:
-			/// This histogram represented by this data.
+			/// The histogram represented by this data.
 			TH1 *histogram;
+
+			/// The draw options for the histogram represented by this data.
+			std::string drawOptions;
 
 		public:
 			/// Default constructor.
-			HistogramData();
-
-			/// Default constructor.
 			HistogramData(TH1 *histogram);
+
+			/// Constructor that allows both the histogram and draw options to
+			/// be given on instantiation.
+			HistogramData(TH1 *histogram, std::string drawOptions);
 
 			/// Gets the histogram.
 			/// @return a pointer to this pair's histogram
 			TH1* getHistogram();
 
+			/// Gets the histogram's draw options.
+			/// @return the draw options associated to the histogram
+			std::string getDrawOptions();
+
 			/// Sets the histogram.
 			/// @param histogram a pointer to this  data's histogram
 			void setHistogram(TH1 *histogram);
+
+			/// Sets the histogram's draw options.
+			/// @param the draw options associated to the histogram
+			void setDrawOptions(std::string drawOptions);
 	};
 }
 #endif
