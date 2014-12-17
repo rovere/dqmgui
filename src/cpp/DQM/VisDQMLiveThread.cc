1ea3c1ee6bd800664a076e41661e44f60d7db83b
Marco Rovere <marco.rovere@cern.ch>
2014-12-17 15:02:08 +0100

    Bug fixes

--- src/cpp/DQM/VisDQMLiveThread.cc
+++ src/cpp/DQM/VisDQMLiveThread.cc
@@ -20,7 +20,6 @@
 
 extern void buildParentNames(StringAtomList & /* to */, const StringAtom & /* from */);
 extern StringAtomTree stree;
-extern "C++" template std::string hexlify<DQMNet::DataBlob>(const DQMNet::DataBlob &);
 
 using namespace lat;
 
