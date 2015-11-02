5917c8211a057d62a972083fe9143c968b47410b
Marco Rovere <marco.rovere@cern.ch>
2015-11-02 10:23:01 +0100

    Restore old icons

--- src/javascript/DQM/Header.js
+++ src/javascript/DQM/Header.js
@@ -204,21 +204,21 @@ GUI.Plugin.DQMHeaderRow = new function() {
        + "<a href='https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftInstructions'>"
        + "shift instructions here</a>.</span>");
     _service = this.heading("Service", "<span>_</span>:", "pickMenu(\"service\")").firstChild;
-    _workspace = this.heading("<img src='" + ROOTPATH + "/static/left_arrow.png'"
+    _workspace = this.heading("<img src='" + ROOTPATH + "/static/previous.png'"
 			      + "style='margin-left:5px;padding-bottom:1px; height: 0.95em' "
 			      + "onclick = 'return GUI.Plugin.DQMHeaderRow.loopWorkspaces(event,\"previous\")' />"
 			      + "&nbsp;Workspace"
-			      + "<img src='" + ROOTPATH + "/static/right_arrow.png'"
+			      + "<img src='" + ROOTPATH + "/static/next.png'"
 			      + "style='margin-left:5px;padding-bottom:1px; height: 0.95em' "
 			      + "onclick = 'return GUI.Plugin.DQMHeaderRow.loopWorkspaces(event,\"next\")' />",
 			      "<span>_</span><span class='delimiter'>&nbsp;&nbsp;.</span>",
 			      "pickMenu(\"workspace\")").firstChild;
 
-    _runNr = this.heading("<img src='" + ROOTPATH + "/static/left_arrow.png'"
+    _runNr = this.heading("<img src='" + ROOTPATH + "/static/previous.png'"
 			      + "style='margin-left:5px;padding-bottom:1px; height: 0.95em' "
 			      + "onclick = 'return GUI.Plugin.DQMHeaderRow.loopRuns(event,\"backward\")' />"
 			      + "&nbsp;Run #"
-			      + "<img src='" + ROOTPATH + "/static/right_arrow.png'"
+			      + "<img src='" + ROOTPATH + "/static/next.png'"
 			      + "style='margin-left:5px;padding-bottom:1px; height: 0.95em' "
 			      + "onclick = 'return GUI.Plugin.DQMHeaderRow.loopRuns(event,\"forward\")' />", "<span>_</span><span class='delimiter'>"
 		          + "&nbsp;&nbsp;.</span>", "pickSample(\"run\", \"dataset\")").firstChild;
